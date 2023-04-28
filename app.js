6da878eb59d940b380ff91d13839c6b4cfaf0541
Daniil <negativdaniil4@gmail.com>
2023-04-28 21:46:32 +0300

    Projec init

---
+++ app.js
@@ -0,0 +1,35 @@
+// №1
+// const myButton = document.getElementById('myButton');
+// myButton.addEventListener('click', function () {
+//    myButton.style.backgroundColor = 'red';
+// });
+
+
+// №2
+// const button = document.createElement('button');
+// button.textContent = 'Натисни мене';
+
+// button.addEventListener('click', () => {
+//    alert('Hello, world!');
+// });
+
+// document.body.appendChild(button);
+
+
+// №3
+// const form = document.getElementById('login-form');
+
+// form.addEventListener('submit', (event) => {
+//    event.preventDefault();
+
+//    const username = form.elements.username.value;
+//    const password = form.elements.password.value;
+
+//    const correctPassword = 'secret';
+
+//    if (password === correctPassword) {
+//       alert(`Вітаємо ${username}! Вхід авторизовано!`);
+//    } else {
+//       alert('Неправильний пароль! Спробуйте ще раз.');
+//    }
+// });
